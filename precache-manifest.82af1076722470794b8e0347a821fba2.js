35e31540fd890714bb03ddedb54243d05a5e8558
Vik-Bert <woodoowanya@gmail.com>
2023-05-10 23:34:08 +0200

    Updates

---
+++ precache-manifest.82af1076722470794b8e0347a821fba2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04ac4d1b9abf17e6f86e803b2224492",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "25403140f799c8f006f5",
+    "url": "/test/static/css/2.90da6880.chunk.css"
+  },
+  {
+    "revision": "e33bfb49a4906c61f660",
+    "url": "/test/static/css/main.65605343.chunk.css"
+  },
+  {
+    "revision": "25403140f799c8f006f5",
+    "url": "/test/static/js/2.120a0f45.chunk.js"
+  },
+  {
+    "revision": "6120b983a03909b7d2cd9bf134169811",
+    "url": "/test/static/js/2.120a0f45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e33bfb49a4906c61f660",
+    "url": "/test/static/js/main.f364f1c0.chunk.js"
+  },
+  {
+    "revision": "3a896537ed486ebf8071",
+    "url": "/test/static/js/runtime-main.f36324a9.js"
+  }
+]);
